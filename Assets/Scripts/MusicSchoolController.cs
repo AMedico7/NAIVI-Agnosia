93f0534823d3323c76a7deefa02473dfeaa9e6cc
Aniol Medico <aniol@olyn.com>
2024-02-22 17:30:12 +0100

    Assets v1

---
+++ Assets/Scripts/MusicSchoolController.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MusicSchoolController : MonoBehaviour
+{
+    
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync("SlidePuzzle");
+    }
+
+
+    public void CloseMap()
+    {
+        SceneManager.LoadSceneAsync("Menu");
+    }
+    
+}
