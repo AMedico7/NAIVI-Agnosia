ce336d082db445bd888f78160531d1b6dc8cdbe3
Aniol Medico <aniol.medico01@estudiant.upf.edu>
2024-02-27 20:47:35 +0100

    Rythm Game

---
+++ Assets/Scripts/RythmGame/BeatScroller.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatScroller : MonoBehaviour
+{
+    public float beatTempo;
+    public bool playing;
+
+    public GameObject[] arrowPrefabs;
+    public Transform[] arrowSpawnPoints;
+    public float spawnInterval;
+
+    public static BeatScroller instance;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        beatTempo = beatTempo / 60f;
+        spawnInterval = beatTempo;
+    }
+
+
+    public void StartPlaying()
+    {
+        playing = true;
+        StartCoroutine(SpawnArrowsCoroutine());
+    }
+
+    IEnumerator SpawnArrowsCoroutine()
+    {
+        while (playing)
+        {
+
+            int index = Random.Range(0, 4);
+
+            GameObject arrowPrefab = arrowPrefabs[index];
+            Transform spawnPoint = arrowSpawnPoints[index];
+
+            GameObject arrow = Instantiate(arrowPrefab, spawnPoint.position, spawnPoint.rotation);
+
+            Coroutine moveCoroutine = StartCoroutine(MoveArrowCoroutine(arrow));
+
+            NoteObject noteObject = arrow.GetComponent<NoteObject>();
+            noteObject.SetMoveCoroutine(moveCoroutine);
+
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+
+
+    IEnumerator MoveArrowCoroutine(GameObject arrow)
+    {
+        while (playing && arrow != null && arrow.activeSelf)
+        {
+
+            arrow.transform.Translate(Vector3.down * beatTempo * Time.deltaTime, Space.World);
+            yield return null;
+        }
+    }    
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/Scripts/RythmGame/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public AudioSource music;
+    public bool startPlaying;
+    public BeatScroller beatScroller;
+
+    public int score;
+
+    public static GameManager instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!startPlaying)
+        {
+            if(Input.anyKeyDown)
+            {
+                startPlaying = true;
+                beatScroller.StartPlaying();
+
+                music.Play();
+            }
+        }
+
+
+        if (score >= 300){
+            StateNameController.GoToDestination();
+        }
+    }
+
+
+    public void NoteHit()
+    {
+        Debug.Log("HIT");
+        score += 100;
+    }
+
+    public void NoteMissed()
+    {
+        Debug.Log("MISS");
+    }
+}
