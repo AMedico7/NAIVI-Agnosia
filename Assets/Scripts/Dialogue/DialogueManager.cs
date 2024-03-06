403e273738aa63492414f0900508ffabc008055b
Aniol Medico <aniol.medico01@estudiant.upf.edu>
2024-03-06 04:38:28 +0100

    Dialogue Scene

---
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DialogueManager : MonoBehaviour
+{
+
+    public int location = 0;
+    public GameObject dialogueBox;
+    public GameObject background;
+    private Dialogue dialogue;
+    private Image backgroundImage;
+
+
+    public Sprite[] backgrounds;
+    public static DialogueManager instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;   
+        // Set dialogue and dialogue lines
+        dialogue = dialogueBox.GetComponent<Dialogue>();
+        backgroundImage = background.GetComponent<Image>();
+    }
+
+    public void SetDialogueLines(string[] newLines)
+    {
+        dialogue.lines = newLines;
+        dialogue.index = 0;
+    }
+}
